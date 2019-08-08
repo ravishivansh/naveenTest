89bc3447c038218fb6f008953179de3beb4d82de
SHIV <SHIV@DESKTOP-H3QN7KC>
2019-08-09 00:14:09 +0530

    test

---
+++ src/main/java/MAVENTEST/TEST/App.java
@@ -0,0 +1,13 @@
+package MAVENTEST.TEST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
